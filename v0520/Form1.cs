897599f19d00a8ad812b7b4cf8011bec40ec643d
真島　祐志 <dat.gp1219@gmail.com>
2021-05-20 12:15:07 +0900

    Initial commit

---
+++ v0520/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace v0520
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top - 10;
+            //label1.Top -= 10;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            MessageBox.Show("こんにちは");
+
+            MessageBox.Show(100.ToString());
+            MessageBox.Show("" + 100);
+            MessageBox.Show($"{100}");
+
+            MessageBox.Show(3.14f.ToString());
+            MessageBox.Show("" + 3.14f);
+            MessageBox.Show($"{3.14f}");
+
+            MessageBox.Show((1 + 1).ToString());
+            MessageBox.Show("" + (1 + 1));
+            MessageBox.Show($"{1 + 1}");
+
+            MessageBox.Show((1.23f * 2.34f).ToString());
+            MessageBox.Show("" + (1.23f * 2.34f));
+            MessageBox.Show($"{1.23f * 2.34f}");
+
+            MessageBox.Show("1+1");
+            MessageBox.Show("1" + "1");
+        }
+        
+        private void button4_Click_1(object sender, EventArgs e)
+        {
+            label1.Top = label1.Top + 10;
+            //label1.Top += 10;
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left + 10;
+            //label1.Left += 10;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            label1.Left = label1.Left - 10;
+            //label1.Left -= 10;
+        }
+    }
+}
